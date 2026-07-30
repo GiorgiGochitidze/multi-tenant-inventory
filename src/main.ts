b32dde1613162291be15314167c1e10a95250cdd
Giorgi Gochitidze <giorgigochitidze555@gmail.com>
2026-07-30 22:33:41 +0400

    fix: fixed incorrect url in frontend url path

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3001', 'multi-tenant-ge.vercel.app'],
+    origin: ['http://localhost:3001', 'https://multi-tenant-ge.vercel.app'],
     credentials: true,
   });
 
